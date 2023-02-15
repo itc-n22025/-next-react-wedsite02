d24b5321f3ed072869484b402e4d881994ff01cd
itc-n22025 <n22025@std.it-college.co.jp>
2023-02-15 06:46:08 +0000

    first commit

---
+++ components/contact.js
@@ -0,0 +1,10 @@
+import styles from 'styles/contact.module.css'
+
+export default function Contact () {
+  return (
+    <div classNamei={styles.stack}>
+      <h3 className={styles.heading}>Contact</h3>
+      <address>cube@web.mail.address</address>
+    </div>
+  )
+}
